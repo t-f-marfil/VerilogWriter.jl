15e87044ff41c2606f112771874a6fa0b6bbadbf
marfil <tfmaril@gmail.com>
2022-07-31 09:36:09 +0900

    slice and bit select done.

--- src/baseutils.jl
+++ src/baseutils.jl
@@ -1,3 +1,19 @@
 function Base.convert(::Type{String}, x::Symbol)
     string(x)
 end
+
+"""
+    Base.iterate(x::Wireexpr)
+
+Needed to deal with some return value that may be 
+either Wireexpr or Vector{Wireexpr}.
+May cause type instability, but it is already inevitable
+in parsing AST.
+"""
+function Base.iterate(x::Wireexpr)
+    (x, nothing)
+end
+
+function Base.iterate(x::Wireexpr, ::Nothing)
+    nothing
+end
