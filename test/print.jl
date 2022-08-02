491549de3ec5deab7a5ee65201bbd734a8510221
marfil <2000110205t.fukami@gmail.com>
2022-08-02 19:10:20 +0900

    add wireexpr literal with bit width.

--- test/print.jl
+++ test/print.jl
@@ -1,4 +1,4 @@
-# portoneline 
+# Oneport
 # [@in/@out]
 # [wire/reg/logic/None]
 # [num/None] 
@@ -24,3 +24,43 @@ c = @portoneline @in logic din1, din2, din3
 
 d = @portoneline @out 10 dout 
 @test string.(d) == ["output [9:0] dout"]
+
+
+# Ports
+# muliple lines
+e = @ports (
+    @in 8 din;
+    @out logic dout1, dout2
+)
+@test string(e) == """(
+    input [7:0] din,
+    output logic dout1,
+    output logic dout2
+);
+"""
+
+# one line
+f = @ports (
+    @in clk
+)
+@test string(f) == """(
+    input clk
+);
+"""
+
+
+# Wireexpr
+
+w = @wireexpr (a + b) << (c - d) >> e & f
+@test string(w) == "((((a + b) << (c - d)) >> e) & f)"
+
+w = @wireexpr &(a) | ~(^(b) ^ -c) == |(d)
+@test string(w) == "((&(a) | ~(^(b) ^ -c)) == |(d))"
+
+w = @wireexpr (0x12 <= 0b100) && (10 < x[10:1]) || y[z:1]
+@test string(w) == "(((18 <= 4) && (10 < x[10:1])) || y[z:1])"
+
+w = w = Wireexpr(32, 5)
+@test string(w) == "32'd5"
+
+# Alassign 
