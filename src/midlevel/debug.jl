7dc9d39da1c1a7789db340977e78b6d04b43377e
marfil <74968391+t-f-marfil@users.noreply.github.com>
2023-07-30 22:06:11 +0900

    add methods to lift debugging ports to the top module

---
+++ src/midlevel/debug.jl
@@ -0,0 +1,16 @@
+function debugNamegen(s::AbstractString)
+    return string("_debug_", s)
+end
+"""
+    debugAdd!(m::Midmodule, n::AbstractString, wid=-1)
+
+Add to `m::Midmodule` a port for debugging.
+"""
+function debugAdd!(m::Midmodule, n::AbstractString, wid=-1)
+    dname = debugNamegen(n)
+    dbgpdecl = @oneport @out @logic $wid $dname
+    dbgal = @always $dname = $n
+
+    vpush!.(m, (dbgpdecl, dbgal))
+    return nothing
+end
