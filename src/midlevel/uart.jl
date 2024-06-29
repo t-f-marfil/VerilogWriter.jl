8da0b261734b39d455ca79d9d8531b24b3057aba
marfil <74968391+t-f-marfil@users.noreply.github.com>
2024-06-29 23:46:20 +0900

    UART recv module now return to initial state at the middle of stop bit

--- src/midlevel/uart.jl
+++ src/midlevel/uart.jl
@@ -1,5 +1,3 @@
-const STOPBIT_MARGIN = 0.9
-
 """
     uartRecv(baudrate, clkfreq; name="UARTRecv")
 
@@ -16,14 +14,15 @@ function uartRecv(baudrate, clkfreq; name="UARTRecv")
     cycleperbitRaw = clkfreq / baudrate
     cycleperbit = Int(round(cycleperbitRaw)) - 1
     cycleperhalfbit = Int(round(cycleperbitRaw / 2)) - 1
-    cycleperbit > 0 && cycleperhalfbit > 0 || error("cycleper(|half)bit should be positive")
+    cycleperbit > 0 && cycleperhalfbit > 0 || error("cycleper(|half)bit should be positive, perbit: $cycleperbit, perhalf: $cycleperhalfbit, where baud = $baudrate, freq = $clkfreq")
     cycleperbit != cycleperhalfbit || error("cycles for a bit and half a bit is the same, increase cycles per bit.")
     
-    cycleperstop = Int(round(cycleperbitRaw*STOPBIT_MARGIN)) - 1
+    cycleperstop = Int(round(cycleperbitRaw / 2)) - 1
 
     rxprts = @ports (
         @in rx;
-        @out @reg 8 dout
+        @out @reg 8 dout;
+        @out @logic outValid
     )
 
     @sym2wire cyclecount, bitcount, nextbyte
@@ -66,7 +65,7 @@ function uartRecv(baudrate, clkfreq; name="UARTRecv")
         end
     )
     alil = @always (
-        $(imcontrolDownstream(imvalid)) = (recvstate == sstop) & $counthalf
+        outValid = (recvstate == sstop) & $counthalf
     )
 
     vpush!.(m, (rxprts, rxfsm, alnextbyte, alcounters, aldout, alil))
@@ -82,7 +81,9 @@ function uartSend(baudrate, clkfreq; name="UARTSend")
 
     txprts = @ports (
         @in 8 din;
-        @out @logic tx
+        @out @logic tx;
+        @in inValid;
+        @out @logic inUpdate
     )
 
     @sym2wire acceptNext, bitcount, cyclecount
@@ -90,7 +91,7 @@ function uartSend(baudrate, clkfreq; name="UARTSend")
 
     txfsm = @FSM sendstate sidle, sstart, sdata, sstop
     transadd!(txfsm, [
-        (Wireexpr(imcontrolUpstream(imvalid)), @tstate sidle => sstart),
+        (@wireexpr(inValid), @tstate sidle => sstart),
         (countfull, @tstate sstart => sdata),
         (countfull & (bitcount == Wireexpr(3, 7)), @tstate sdata => sstop),
         (countfull & acceptNext, @tstate sstop => sstart),
@@ -112,7 +113,7 @@ function uartSend(baudrate, clkfreq; name="UARTSend")
     )
     alaccept = @always (
         acceptNext = 0;
-        if $(imcontrolUpstream(imvalid))
+        if inValid
             if sendstate == sidle
                 acceptNext = 1
             elseif (sendstate == sstop) && (cyclecount == $cycleperbit)
@@ -134,7 +135,7 @@ function uartSend(baudrate, clkfreq; name="UARTSend")
         end
     )
     alupdate = @always (
-        $(imcontrolUpstream(imupdate)) = (
+        inUpdate = (
             ((sendstate == sstop) && (cyclecount == $cycleperbit))
             || sendstate == sidle
         )
